2709e22aac50c531cf9529c18bec2158c7e89a0e
Dimitri Temkin <dimitri.temkin@gmail.com>
2017-02-09 16:08:23 +1100

    mplement Queue in Pseudoclassical Pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,23 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.front = 0;
+  this.back = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.back] = value;
+  this.back++;
+};
 
+Queue.prototype.dequeue = function() {
+  if (this.back - this.front > 0) {
+    var dequeuedItem = this.storage[this.front];
+    delete this.storage[this.front];
+    this.front++;
+    return dequeuedItem;
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.back - this.front;
+};
