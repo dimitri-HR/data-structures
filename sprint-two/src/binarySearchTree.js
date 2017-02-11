083256d549bcde0f02ffa704524a03f515a20c24
Dimitri Temkin <dimitri.temkin@gmail.com>
2017-02-12 06:22:29 +1100

    Implement BinarySearchTree - insert

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,26 @@
 var BinarySearchTree = function(value) {
+  var obj = Object.create(BinarySearchTree.prototype);
+  obj.value = value;
+  obj.left = null;
+  obj.right = null;
+  return obj;
+};
+
+BinarySearchTree.prototype.insert = function(value) {
+  var newNode = BinarySearchTree(value);
+
+  var insertNode = function (node) {
+    if (value < node.value && !node.left) {
+      node.left = newNode;
+    } else if (value < node.value) {
+      insertNode(node.left);
+    } else if (value > node.value && !node.right) {
+      node.right = newNode;
+    } else if (value > node.value) {
+      insertNode(node.right);
+    }
+  };
+  insertNode(this);
 };
 
 
