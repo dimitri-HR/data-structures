f732c9750621b8ef4878f99a58af16170b107b32
Dimitri Temkin <dimitri.temkin@gmail.com>
2017-02-11 14:14:10 +1100

    Implement set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,32 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
+  var targetIndex = this._storage.indexOf(item);
+  if (targetIndex !== -1) {
+    this._storage.splice(targetIndex, 1);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// add function:  Complexity O(n) - linear
+// contains function:  Complexity O(n) - linear
+// remove function: Complexity O(n) - linear
