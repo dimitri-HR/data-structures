cbedf50f37e8ca5dcd1205c9434f33a529e3f7cf
Dimitri Temkin <dimitri.temkin@gmail.com>
2017-02-12 18:02:52 +1100

    Refactor hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,26 +6,42 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // retrieve a bucket
   var bucket = this._storage.get(index);
-  if (bucket === undefined) {
-    this._storage.set(index, []);
+  if (!bucket) {
+    bucket = [];
+    this._storage.set(index, bucket);
   }
-  bucket = this._storage.get(index);
-  bucket.push([k, v]);
+  var found = false;
+  // iterate over bucket
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    // if key exist
+    if (tuple[0] === k) {
+      // update it
+      tuple[1] = v;
+      found = true;
+      break;
+    }
+  }
+  // if key not found
+  if (!found) {
+    // insert a new tuple
+    bucket.push([k, v]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
-  var item;
   if (bucket) {
-    bucket.forEach(el => {
-      if (el[0] === k) {
-        item = el[1];
+    for (var i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i];
+      if (tuple[0] === k) {
+        return tuple[1];
       }
-    });
+    }
   }
-  return item;
 };
 
 HashTable.prototype.remove = function(k) {
@@ -39,8 +55,10 @@ HashTable.prototype.remove = function(k) {
     var tuple = bucket[i];
     if (tuple[0] === k) {
       bucket.splice(i, 1);
+      return tuple[1];
     }
   }
+  return null;
 };
 
 /*
@@ -50,3 +68,22 @@ HashTable.prototype.remove = function(k) {
  // insert function - Complexity O(1) - constant
  // retrieve function - Complexity O(1) - constant
  // remove function - Complexity O(1) - constant
+
+
+
+// ************************************************
+ // Notes: Hash Table
+ // limitedArray structure
+ // limitedArray = {
+ //  storage:
+ //    [ // using hashed indexes instead of numbers 0, 1, 2...
+ //      [ // bucket arr
+ //        [k, v], [k, v]  // tuples arr
+ //      ],
+ //      [ ['cat', 'soft'], ['dog', 'bark'] ] // e.g
+ //    ],
+ //  get: function() {},
+ //  set: function() {},
+ //  each: function() {}
+ // }
+ // ************************************************
